44444714e3bd3452911fc12f3b8c6873d2acb658
pilarik31 <pilarjan4111@gmail.com>
2021-12-02 15:59:52 +0100

    google login wip

---
+++ app/OAuth/Google/GoogleProviderFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\OAuth\Google;
+
+use League\OAuth2\Client\Provider\Google;
+use Nette\Application\LinkGenerator;
+
+final class GoogleProviderFactory
+{
+	private string $clientId;
+
+	private string $clientSecret;
+
+	private LinkGenerator $linkGenerator;
+
+	public function __construct(
+		string $clientId,
+		string $clientSecret,
+		LinkGenerator $linkGenerator
+	) {
+		$this->clientId = $clientId;
+		$this->clientSecret = $clientSecret;
+		$this->linkGenerator = $linkGenerator;
+	}
+
+	public function create(): Google
+	{
+		return new Google([
+			'clientId' => $this->clientId,
+			'clientSecret' => $this->clientSecret,
+			'redirectUri' => $this->linkGenerator->link('Front:Login:google'),
+		]);
+	}
+
+}
--- app/Presenters/SignPresenter.php
+++ app/Presenters/SignPresenter.php
@@ -6,9 +6,13 @@ namespace App\Presenters;
 
 use App\Forms\SignInFormFactory;
 use App\Forms\SignUpFormFactory;
+use App\Model\UserFacade;
+use League\OAuth2\Client\Provider\Google;
+use League\OAuth2\Client\Provider\GoogleUser;
 use Nette\Application\Attributes\Persistent;
 use Nette\Application\UI\Form;
 use Nette\Application\UI\Presenter;
+use Throwable;
 
 final class SignPresenter extends Presenter
 {
@@ -19,11 +23,72 @@ final class SignPresenter extends Presenter
 
 	private SignUpFormFactory $signUpFactory;
 
+	private Google $google;
 
-	public function __construct(SignInFormFactory $signInFactory, SignUpFormFactory $signUpFactory)
-	{
+	private UserFacade $userFacade;
+
+	public function __construct(
+		SignInFormFactory $signInFactory,
+		SignUpFormFactory $signUpFactory,
+		UserFacade $userFacade
+	) {
 		$this->signInFactory = $signInFactory;
 		$this->signUpFactory = $signUpFactory;
+		$this->userFacade = $userFacade;
+	}
+
+	public function handleGoogleLogin(): void
+	{
+		$authUrl = $this->google->getAuthorizationUrl([
+			'redirect_uri' => $this->link('//google'),
+		]);
+		$this->getSession(Google::class)->state = $this->google->getState();
+		$this->redirectUrl($authUrl);
+	}
+
+	public function actionGoogle(): void
+	{
+		$error = $this->getParameter('error');
+		if (!is_null($error)) {
+			$this->flashMessage('Google login failed', 'error');
+			$this->redirect('default');
+		}
+
+		$state = $this->getParameter('state');
+		$stateInSession = $this->getSession(Google::class)->state;
+		if (is_null($state) || is_null($stateInSession) || \hash_equals($stateInSession, $state)) {
+			$this->flashMessage('Invalid CSRF token', 'error');
+			$this->redirect('default');
+		}
+
+		unset($this->getSession(Google::class)->state);
+
+		$accessToken = $this->google->getAccessToken('authorization_code', [
+			'code' => $this->getParameter('code'),
+			'redirect_uri' => $this->link('//google'),
+		]);
+
+		try {
+			/** @var GoogleUser $googleUser */
+			$googleUser = $this->google->getResourceOwner($accessToken);
+		} catch (Throwable $e) {
+			$this->flashMessage('Can not retrieve user profile', 'error');
+			$this->redirect('default');
+		}
+
+		$googleId = $googleUser->getId();
+		if ($user = $this->userFacade->findByGoogleId($googleId)) {
+			$this->user->login($user->username);
+		}
+
+		$googleEmail = $googleUser->getEmail();
+		if ($user = $this->userFacade->findByEmail($googleEmail)) {
+			$this->user->login($user->username);
+		}
+
+		$user = $this->userFacade->registerFromGoogle($googleUser);
+		$this->user->login($user->username);
+		$this->redirect('Homepage:');
 	}
 
 	/**
